9244c8505bbe65c06894a417da1f4e38899c1c6e
mrafeie <m.rafeie.mr@gmail.com>
2026-09-16 14:44:19 +0100

    Implement order total calculation and display
    Added logging for order details and total calculation.

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,11 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+console.log("|QTY|      |Item|               |Total|");
+let total = 0;
+for(let item of order){
+  const { itemName, quantity, unitPricePence } = item;
+  const itemTotal = quantity * unitPricePence;
+  total += itemTotal;
+  console.log(`${quantity.toString().padEnd(8)}${itemName.padEnd(20)}${(itemTotal / 100).toFixed(2)}`);
+}
